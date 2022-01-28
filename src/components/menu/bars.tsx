5515ade4d9860f6cbf101bd9dfde3296ef57314c
TyfangXV <9fangdev@gmail.com>
2022-01-28 23:47:45 +0400

    added menu bar

--- src/components/menu/bars.tsx
+++ src/components/menu/bars.tsx
@@ -1,24 +1,21 @@
-import Image from 'next/image';
 import React from 'react';
 import styles from '../../styles/components/menu/bar.module.css';
-import { IoLogoAppleAppstore } from 'react-icons/io5';
-import { BsFillTelephoneFill } from 'react-icons/bs';
-export const RightVerticalBar = () => {
+
+interface VerticalBarProps {
+  children: React.ReactNode,
+  clip_path : string,
+  background_image : string,
+}
+
+
+export const VerticalBar = ({clip_path, children, background_image}:VerticalBarProps) => {  
   return (
-    <div className={styles.right_vertical_bar}>
-      <div className={styles.right_vertical_bar_black_screen}></div>
-      <p className={styles.title}>store</p>
-      <IoLogoAppleAppstore className={styles.icon} />
+    <div className={styles.right_vertical_bar} style={{clipPath : clip_path, backgroundImage : background_image}}>
+      <div className={styles.right_vertical_bar_black_screen} style={{clipPath : clip_path}}>
+        {children}
+      </div>
     </div>
   );
 };
 
-export const LeftVerticalBar = () => {
-  return (
-    <div className={styles.left_vertical_bar}>
-      <div className={styles.left_vertical_bar_black_screen}></div>
-        <p className={styles.title}>contact</p>          
-      <BsFillTelephoneFill className={styles.icon} />
-    </div>
-  );
-};
+
--- src/components/menu/menuscreen.tsx
+++ src/components/menu/menuscreen.tsx
@@ -1,46 +1,76 @@
 import React from 'react';
 import styles from '../../styles/components/menu/menuscreen.module.css';
+import Barstyles from '../../styles/components/menu/bar.module.css';
 import { motion, useAnimation } from 'framer-motion';
 import { FiMenu } from 'react-icons/fi';
 import { isMenuOpen } from '../../state/menu';
 import { useRecoilState } from 'recoil';
-import { LeftVerticalBar, RightVerticalBar } from './bars';
+import { VerticalBar } from './bars';
+import { IoLogoAppleAppstore } from 'react-icons/io5';
+import { MdLocalPhone}  from 'react-icons/md'
 
 const MenuScreen = () => {
-    const [menuOpen, setMenuOpen] = useRecoilState(isMenuOpen);
-    const startAnimationControls = useAnimation();
-    const endAnimationControls = useAnimation();
+  const [menuOpen, setMenuOpen] = useRecoilState(isMenuOpen);
+  const startAnimationControls = useAnimation();
 
-    startAnimationControls.start({
-        y: menuOpen ? 0 : '-100%',
-        transition: {
-            duration: 0.5,
-            ease: 'easeInOut',
-        },
-    })
+  startAnimationControls.start({
+    y: menuOpen ? 0 : '-100%',
+    transition: {
+      duration: 0.5,
+      ease: 'easeInOut',
+    },
+  });
 
   return (
     <motion.div
       className={styles.container}
       animate={startAnimationControls}
-      transition={{ duration: 10, ease: "easeIn" }}
+      transition={{ duration: 10, ease: 'easeIn' }}
     >
-        <span className={styles.topRight} style={{ marginTop: '2%', marginRight: '1%' }}>
-          <FiMenu
-            className={styles.menu_btn}
-            onClick={() => setMenuOpen(!menuOpen)}
-          />
-        </span>
-        <div className={styles.menu_btn_container}>
-          <RightVerticalBar/>
-          <LeftVerticalBar/>          
-        </div>
+      <span
+        className={styles.topRight}
+        style={{ marginTop: '2%', marginRight: '1%' }}
+      >
+        <FiMenu
+          className={styles.menu_btn}
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+      </span>
+      <div className={styles.menu_btn_container}>
+
+        <VerticalBar clip_path={'polygon(0 0, 100% 10%, 100% 100%, 0 calc(100% - 5vw))'} background_image={'url("/store.jpg")'}>
+          <span className={Barstyles.alignUp}>  
+            <p className={Barstyles.title}>store</p>
+          </span>
+          <span className={Barstyles.alignBottom}>
+            <IoLogoAppleAppstore className={Barstyles.icon} />
+          </span>
+        </VerticalBar>
 
 
+
+        <VerticalBar clip_path={'polygon(0% 10%,100% 0, 100% calc(100% - 5vw), 0  100%)'} background_image={'url("/contact.jpg")'}>
+          <span className={Barstyles.alignUp}>    
+            <p className={Barstyles.title}>contact</p>
+          </span>
+          <span className={Barstyles.alignBottom}>
+            <MdLocalPhone className={Barstyles.icon} />
+          </span>
+        </VerticalBar>
+
+
+        <VerticalBar clip_path={'polygon(0% 0%,100% 0, 100% calc(100% - 5vw), 0  calc(100% - 5vw))'} background_image={'url("/about-us.jpg")'}>
+          <span className={Barstyles.alignUp}>    
+            <p className={Barstyles.title}>contact</p>
+          </span>
+          <span className={Barstyles.alignBottom}>
+            <MdLocalPhone className={Barstyles.icon} />
+          </span>
+        </VerticalBar>
+      </div>
     </motion.div>
   );
 };
-
 
 /*
 
